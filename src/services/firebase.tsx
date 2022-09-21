c80312d805dbfcc41bdc99d21fb12cd64537592d
thiago <ts_muniz@outlook.com>
2022-09-20 21:26:32 -0300

    feat: adiciona a conexão com o firebase

---
+++ src/services/firebase.tsx
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbXe1_-g0XrDqw0Uy_zFd2QnvsV8OsiHE",
+  authDomain: "boardapp-391eb.firebaseapp.com",
+  projectId: "boardapp-391eb",
+  storageBucket: "boardapp-391eb.appspot.com",
+  messagingSenderId: "649422106510",
+  appId: "1:649422106510:web:5e2d4e06f51fe9d3f03edf",
+  measurementId: "G-4DX6608SKL"
+};
+
+export const app = initializeApp(firebaseConfig);
